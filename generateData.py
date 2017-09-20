d464c4af0b6fa81013f1ed2ead9c79f6814588da
root <root@praiavermelha.gta.ufrj.br>
2017-09-20 14:35:12 -0300

    now generating a line of measures

--- generateData.py
+++ generateData.py
@@ -3,4 +3,22 @@ import time
 
 random.seed(time.time())
 
-temperature = random.uniform(20.0, 50.0)
+class Data:
+    def __init__(self):
+        self.latitude = random.uniform(-22.3,-22.6)
+        self.longitude = random.uniform(-43.3,-43.6)
+        self.light = random.uniform (997,999)
+        self.temperature = random.uniform(20.0, 50.0)
+        self.humidity = random.uniform (28.0, 31.0)
+        self.rain = random.uniform (774, 781)
+
+def createSensingPost (data, line):
+    data.append(Data ())
+    #print '[',data.latitude,',',data.longitude,',',data.light,',',data.temperature,',',data.humidity,',',data.rain,']'
+
+i = 0
+data = []
+while (i < 20):
+    createSensingPost(data, i)
+
+print data
