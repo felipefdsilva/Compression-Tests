9f76a21020124690b66fe7cfb967026f560f32c3
felipefdsilva <felipeferreira@poli.ufrj.br>
2017-09-25 16:11:14 -0300

    now doing POSTs

--- generateData.py
+++ generateData.py
@@ -1,25 +1,63 @@
 import random
 import time
+import datetime
+import json
+import requests
+import zlib
+
+#SERVER_CERTS = '/home/felipe/ssl/XX.cert.pem' #Server_autorization
+URL = "https://sensingbus.gta.ufrj.br/zip_measurements_batch_sec/"
+STOP_ID = 1
+
+PRIMARY_KEY='/home/felipe/ssl/raspberry3.key.pem'
+LOCAL_CERTIFICATE='/home/felipe/ssl/raspberry3.cert.pem'
 
 random.seed(time.time())
 
+message = {}
+
 class Data:
     def __init__(self):
+        self.datetime = str(datetime.datetime.now().strftime('%d%m%y%H%M%S%f'))[0:12] + '00'
         self.latitude = random.uniform(-22.3,-22.6)
         self.longitude = random.uniform(-43.3,-43.6)
-        self.light = random.uniform (997,999)
+        self.light = random.randint (997,999)
         self.temperature = random.uniform(20.0, 50.0)
         self.humidity = random.uniform (28.0, 31.0)
-        self.rain = random.uniform (774, 781)
+        self.rain = random.randint (774, 781)
 
-def createSensingPost (data, line):
-    data.append(Data ())
-    #print '[',data.latitude,',',data.longitude,',',data.light,',',data.temperature,',',data.humidity,',',data.rain,']'
+def createMessage():
+    message["stop_id"] = STOP_ID
+    message["batches"] = []
+    sensingMessage = {}
+    dataLine = []
+    data = []
 
-i = 0
-data = []
-while (i < 20):
-    createSensingPost(data, i)
-    dataLine = '['+ str(data[i].latitude) + ',' + str(data[i].longitude) + ',' + str(data[i].light) + ',' + str(data[i].temperature) + ',' + str(data[i].humidity) + ',' + str(data[i].rain) + ']'
-    i+=1
-    print dataLine
+    for sensingNode in range (1, 2):
+        sensingMessage["node_id"] = sensingNode
+        sensingMessage["type"] = 'data'
+        sensingMessage["received"] = str(datetime.datetime.now().strftime('%d%m%y%H%M%S%f'))[0:12] + '00'
+        sensingMessage["header"] = "datetime,lat,lng,light,temperature,humidity,rain"
+
+        for line in range (0, 1):
+            data.append(Data ())
+            dataLine.append(str(data[line].datetime) + ',' + str(data[line].latitude) + ',' + str(data[line].longitude) + ',' + str(data[line].light) + ',' + str(data[line].temperature) + ',' + str(data[line].humidity) + ',' + str(data[line].rain))
+        sensingMessage["load"] = dataLine
+        data = []
+
+        message['batches'].append(sensingMessage)
+
+def doPOST (message):
+        #print 'MENSAGEM COMPRIMIDA'
+        #print message
+        headers = {'Content-Encoding':'text/plain','Content-Length':str(len(message))}
+        r = requests.post('%s'%URL, data=message,headers=headers,cert=(LOCAL_CERTIFICATE, PRIMARY_KEY))
+        print r.text
+        #print r.json
+        #print r
+
+if __name__ == "__main__":
+    createMessage ()
+    message = zlib.compress(json.dumps(message).encode('utf-8').encode('zlib_codec'), 9)
+    #print message
+    doPOST(message)
